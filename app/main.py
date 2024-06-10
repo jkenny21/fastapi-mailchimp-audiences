23eecaa1ec429d6c2556def9e4a09fa8eade1e84
Jalen Kenny <jtkenny21@gmail.com>
2024-06-09 19:32:23 -0700

    initial mailchimp integration with audience + contact creation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+
 from fastapi import HTTPException, FastAPI
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing.api_client import ApiClientError
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -24,6 +32,66 @@ def on_startup():
 
 def raise_user_404():
     raise HTTPException(status_code=404, detail="User not found")
+
+@app.post("/mailchimp-audiences/")
+def create_audience():
+    mailchimp = MailchimpMarketing.Client()
+    mailchimp.set_config({
+    "api_key": os.environ.get("MAILCHIMP_API_KEY"),
+    "server":  os.environ.get("MAILCHIMP_SERVER_PREFIX")
+    })
+
+    body = {
+    "permission_reminder": "You signed up for updates on our website",
+    "email_type_option": False,
+    "campaign_defaults": {
+        "from_name": "Jalen",
+        "from_email": "jtkenny21@gmail.com",
+        "subject": "Mailchimp Audience Test",
+        "language": "EN_US"
+    },
+    "name": "Mailchimp Audience Test",
+    "contact": {
+        "company": "Jalen",
+        "address1": "1234 West 1st Ave",
+        "city": "Vancouver",
+        "state": "BC",
+        "zip": "V7J5D5",
+        "country": "CA"
+    }
+    }
+
+    try:
+      response = mailchimp.lists.create_list(body)
+      print("Response: {}".format(response))
+    except ApiClientError as error:
+      print("An exception occurred: {}".format(error.text))
+
+@app.post("/mailchimp-contacts")
+def create_contact():
+    mailchimp = MailchimpMarketing.Client()
+    mailchimp.set_config({
+    "api_key": os.environ.get("MAILCHIMP_API_KEY"),
+    "server":  os.environ.get("MAILCHIMP_SERVER_PREFIX")
+    })
+
+    list_id = os.environ.get("MAILCHIMP_AUDIENCE_ID")
+
+    member_info = {
+        "email_address": "jtkenny21+testcontact@gmail.com",
+        "status": "pending",
+        "merge_fields": {
+        "FNAME": "Jalen",
+        "LNAME": "Kenny"
+        }
+    }
+
+    try:
+        response = mailchimp.lists.add_list_member(list_id, member_info)
+        print("response: {}".format(response))
+    except ApiClientError as error:
+        print("An exception occurred: {}".format(error.text))
+
 
 @app.post("/users/", response_model=User)
 def create_user(user: User):
